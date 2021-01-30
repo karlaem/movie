8929a3509f7318f623fc33a8dec520245b5ee3b3
karlaem <60242444+karlaem@users.noreply.github.com>
2021-01-29 19:47:50 -0800

    I got the details

--- src/Title.js
+++ src/Title.js
@@ -3,28 +3,59 @@ import React, { Component} from 'react';
 const credentials = require('./credentials.json');
 
 class Title extends Component{
+
+  state = {
+    loading: true,
+    movie: null
+  };
   
   constructor(props){
     super(props)
     //console.log(props)
     //console.log(this.props.match.params.movieID)
-
-    this.performSearch(this.props.match.params.movieID) 
+    let id = this.props.match.params.movieID
+    this.searchmovie(id) 
   }
-  performSearch(searchID) {
-    console.log("search using moviedb");
-    console.log(searchID);
-    const urlmovies ='http://www.omdbapi.com/?apikey='+credentials.imdb.apikey+'&i='+ searchID;
-    fetch(urlmovies)
-    .then(response => response.json())
-    .then(data => console.log(data));
+
+  async searchmovie(id) {
+    console.log("search using searchovie");
+    const url = 'http://www.omdbapi.com/?apikey='+credentials.imdb.apikey+'&i='+ id;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    this.setState({ movie: data, loading: false });
   }
 
   render() {
+    if (this.state.loading) {
+      return <div>loading...</div>;
+    }
+
+    if (!this.state.movie) {
+      return <div>didn't get a movie</div>;
+    }
     return (
     <div className="App">
-        <h1>Movie Detail fetch</h1>
-        {this.props.match.params.movieID}
+      <a href="/">Back</a>
+       <div className="poster">
+        <img
+          width="200"
+          src={this.state.movie.Poster}
+          alt={this.state.movie.Title}
+        />
+      </div>
+      <div className="details">
+        <h1>{this.state.movie.Title}</h1>
+        {/*this.props.match.params.movieID*/}
+        <p>IMDb Rate: {this.state.movie.imdbRating}</p>
+        <p>Director: {this.state.movie.Director}</p>
+        <p>Year: {this.state.movie.Year}</p>
+        <p>Genre: {this.state.movie.Genre}</p>
+        <p>Released: {this.state.movie.Released}</p>
+        <p>Runtime: {this.state.movie.Runtime}</p>
+        <p>Actors: {this.state.movie.Actors}</p>
+        <p>{this.state.movie.Plot}</p>
+      </div>
     </div>
     );
   }
